7d803e1e1b09dbe9270f57fddcc614ab6fc7abad
z9877969 <z9877969@ukr.net>
2025-06-02 12:17:48 +0300

    controled form

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,101 +1,112 @@
-// import clsx from 'clsx';
-import { useState } from 'react';
+import { useId, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import clsx from 'clsx';
 import Button from '../Button/Button';
 import s from './TodoForm.module.css';
 
 const TodoForm = () => {
-  const [descr, setDescr] = useState('');
+  const [form, setForm] = useState({
+    date: '',
+    descr: '',
+    priority: '',
+    theme: '',
+  });
+
+  const lowId = useId();
+  const mediumId = useId();
+  const highId = useId();
 
   const handleChange = (e) => {
-    const { value } = e.target;
+    const { value, name } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-    console.log('value :>> ', value);
-    setDescr(value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = { ...form, isDone: false, id: uuidv4() };
+    console.log(JSON.stringify(formData, null, 2));
   };
 
   return (
-    <form className={s.form}>
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label className={s.label}>
+        <span> Date </span>
+        <input
+          type="date"
+          className={s.input}
+          name="date"
+          value={form.date}
+          onChange={handleChange}
+        />
+      </label>
       <label className={s.label}>
         <span> Description </span>
         <input
           className={s.input}
           type="text"
-          value={descr}
+          value={form.descr}
           name="descr"
           onChange={handleChange}
         />
       </label>
-      <Button size="medium">OK</Button>
-    </form>
-  );
-};
-
-export default TodoForm;
-
-/* Select
-<label className={s.label}>
-        <span> Theme </span>
-        <select name="theme">
-          <option value="green">Green</option>
-          <option value="orange">Orange</option>
-          <option value="red">Red</option>
-        </select>
-      </label>
-*/
-
-/* Date
-<label className={s.label}>
-        <span> Date </span>
-        <input className={s.input} name="date" type="date" />
-      </label>
-*/
-
-/* Description
-<label className={s.label}>
-        <span> Description </span>
-        <input className={s.input} type="text" name="descr" />
-      </label>
-*/
-
-/* 
-<div className={s.labelWrapper}>
+      <div className={s.labelWrapper}>
         <div className={s.radioWrapper}>
           <input
-            id="formRadioLow"
+            id={lowId}
             className={s.input}
             type="radio"
             name="priority"
             value="low"
+            checked={'low' === form.priority}
+            onChange={handleChange}
           />
-          <label
-            className={clsx(s.label, s.radio)} htmlFor="formRadioLow"
-          >
+          <label className={clsx(s.label, s.radio)} htmlFor={lowId}>
             Low
           </label>
         </div>
         <div className={s.radioWrapper}>
           <input
-            id="formRadioMedium"
+            id={mediumId}
             className={s.input}
             type="radio"
             name="priority"
             value="medium"
+            checked={'medium' === form.priority}
+            onChange={handleChange}
           />
-          <label className={clsx(s.label, s.radio)} htmlFor="formRadioMedium">
+          <label className={clsx(s.label, s.radio)} htmlFor={mediumId}>
             Medium
           </label>
         </div>
         <div className={s.radioWrapper}>
           <input
-            id="formRadioHigh"
+            id={highId}
             className={s.input}
             type="radio"
             name="priority"
             value="high"
+            checked={'high' === form.priority}
+            onChange={handleChange}
           />
-          <label className={clsx(s.label, s.radio)} htmlFor="formRadioHigh">
+          <label className={clsx(s.label, s.radio)} htmlFor={highId}>
             High
           </label>
         </div>
       </div>
-*/
+      <label className={s.label}>
+        <span> Theme </span>
+        <select name="theme" value={form.theme} onChange={handleChange}>
+          <option value=""></option>
+          <option value="green">Green</option>
+          <option value="orange">Orange</option>
+          <option value="red">Red</option>
+        </select>
+      </label>
+      <Button type="submit" size="medium">
+        OK
+      </Button>
+    </form>
+  );
+};
+
+export default TodoForm;
