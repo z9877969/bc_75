25cd6862018ee7e0e7db881b604d3e8a9f27c6ac
z9877969 <z9877969@ukr.net>
2025-06-05 10:03:01 +0300

    requests with actions and effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import ArtistsList from './components/ArtistsList/ArtistsList';
 import artistsData from './assets/artists.json';
 import SearchForm from './components/SearchForm/SearchForm';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        'https://sound-wave.b.goit.study/api/artists'
+      );
+
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
+  console.log('RENDER');
+
   return (
     <>
-      <SearchForm />
+      <button
+        type="button"
+        onClick={async () => {
+          const { data } = await axios.get(
+            'https://sound-wave.b.goit.study/api/artists'
+          );
+
+          console.log('data :>> ', data);
+        }}
+      >
+        Get Data
+      </button>
       <ArtistsList artistsList={artistsData} />
+      {/* <SearchForm />
+       */}
     </>
   );
 }
