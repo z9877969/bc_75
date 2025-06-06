5816de59c11d401c909a7d3a0bb45fe296e3fc54
z9877969 <z9877969@ukr.net>
2025-06-06 11:11:29 +0300

    useRef with elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import Header from './components/Header/Header';
 import Todo from './components/Todo/Todo';
 import Artists from './components/Artists/Artists';
 import ComponentUseMemoExample from './components/ComponentUseMemoExample';
+import ComponentUseRefExample from './components/CompoenetUseRefExample';
 
 function App() {
   return (
     <>
       <Header />
-      {/* <ComponentUseMemoExample /> */}
-      <Todo />
+      {/* <Todo /> */}
       {/* <Artists /> */}
+      {/* <ComponentUseMemoExample /> */}
+      <ComponentUseRefExample />
     </>
   );
 }
 
 export default App;
-
 
 // Comp() | lexical env -> Comp() lexical env -> Comp()
